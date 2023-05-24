48e8fe5c2429b01cc28aa6cc7e2a76b18ee3a3fa
fede3300 <federica.signoriello@studio.unibo.it>
2023-05-24 11:23:52 +0200

    fixed homepage mobile issues

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # SoundText
-SoundText è il sito web dove le lyrics prendono vita.
+[SoundText](https://fede3300.github.io/SoundText/index.html) è il sito web dove le lyrics prendono vita.
--- js/background.js
+++ js/background.js
@@ -1,5 +1,11 @@
+/**
+ * https://codepen.io/georgedoescode/pen/XWNmvro
+ * Author: George Francis
+ * (c) 2023 George Francis. All rights reserved.
+ */
+
 import * as PIXI from "https://cdn.skypack.dev/pixi.js@5.x";
-import { KawaseBlurFilter } from "https://cdn.skypack.dev/@pixi/filter-kawase-blur@3.2.0";
+import {KawaseBlurFilter} from "https://cdn.skypack.dev/@pixi/filter-kawase-blur@3.2.0";
 import SimplexNoise from "https://cdn.skypack.dev/simplex-noise@3.0.0";
 import hsl from "https://cdn.skypack.dev/hsl-to-hex";
 import debounce from "https://cdn.skypack.dev/debounce";
